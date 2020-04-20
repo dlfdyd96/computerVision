1bcfecd1435279d367afbdabea252e55550e377e
dlfdyd96 <1yongs_@naver.com>
2020-04-20 11:42:11 +0900

    Linear Regrssion Analysis

---
+++ LinearRegressionAnalysis.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+num_points = 100
+vectors_set = []
+for i in range(num_points):
+    x1 = np.random.normal(0.0, 0.55)
+    y1 = x1 * 0.1 + 0.3 + np.random.normal(0.0, 0.03)
+    vectors_set.append([x1, y1])
+x_data = [v[0] for v in vectors_set]
+y_data = [v[1] for v in vectors_set]
+
+import matplotlib.pyplot as plt
+
+# Graphic Display
+'''
+plt.plot(x_data, y_data, 'ro')
+plt.legend()
+plt.show()
+'''
+
+import tensorflow.compat.v1 as tf
+
+tf.disable_v2_behavior()
+
+W = tf.Variable(tf.random.uniform([1], -1.0, 1.0))
+b = tf.Variable(tf.zeros([1]))
+y = W*x_data + b
+
+loss = tf.math.reduce_mean(tf.square(y - y_data))
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+train = optimizer.minimize(loss)
+
+init = tf.global_variables_initializer()
+
+sess= tf.Session()
+sess.run(init)
+
+for step in range(8):
+    sess.run(train)
+    print(step, sess.run(W), sess.run(b))
+    print(step, sess.run(loss))
+
+    #Graphic display
+    plt.plot(x_data, y_data, 'ro')
+    plt.plot(x_data, sess.run(W)*x_data+sess.run(b))
+    plt.xlabel('x')
+    plt.xlim(-2, 2)
+    plt.ylim(0.1, 0.6)
+    plt.ylabel('y')
+    plt.legend()
+    plt.show()
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # computerVision
 현장에서 사용되는 핵심 비전 알고리즘 소개. 컴퓨터비전 응용에 적용할 수 있는 딥러닝 모델을 중점적으로 다룸.  OpenCV, Tensorflow 등의 공개 라이브러리를 이용한 컴퓨터비전 관련 프로젝트 수행
+
+
+### 환경
+- OS : Window10
+- Python : 3.7.7
+- tensorflow : 2.1.0
+### Memo
+- tf.compat.v1 : Bring in all of the public TensorFlow interface into this module.
+- tf.Variable : https://www.tensorflow.org/api_docs/python/tf/Variable
+- tf.compat.v1.Session : A class for running TensorFlow operations.
+- tf.math.reduce_mean : Computes the mean of elements across dimensions of a tensor.
+- tf.math.square : Computes square of x element-wise.
